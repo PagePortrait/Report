0e1402d69ab9c8da88a7502a31628eb460cc7e3b
Pavlo <pavlo.berezin@gmail.com>
2016-06-07 16:21:12 +0300

    According to comments

--- tests/testaudience.js
+++ tests/testaudience.js
@@ -6,27 +6,29 @@
  */
 
 
-/** @const {!RegExp} */ var PATTERN = /^\d([,\.:]*\d)*[%k]?$/;
+/** @const {!RegExp} */ var PATTERN = /^\d([,\.:]*\d)*[%km]?$/;
 
 /**
  * @return {boolean} Returns "true" if test failed.
  */
 function testAudience() {
-  /** @type {Array.<string>} */ var nodes = ['audience-engagement-visitors-value',
-               'audience-engagement-pageviews-value',
-               'audience-engagement-pageviews-delta',
-               'audience-engagement-time-on-site-value',
-               'audience-engagement-time-on-site-delta',
-               'audience-engagement-search-visits-value',
-               'audience-engagement-search-visits-delta',
-               'audience-engagement-bounce-rate-value',
-               'audience-engagement-bounce-rate-delta'];
+  /** @type {Array.<string>} */ var nodes = [
+   'audience-engagement-visitors-value',
+   'audience-engagement-pageviews-value',
+   'audience-engagement-pageviews-delta',
+   'audience-engagement-time-on-site-value',
+   'audience-engagement-time-on-site-delta',
+   'audience-engagement-search-visits-value',
+   'audience-engagement-search-visits-delta',
+   'audience-engagement-bounce-rate-value',
+   'audience-engagement-bounce-rate-delta'
+  ];
   /** @type {number} */ var length = nodes.length;
   /** @type {Element} */ var element;
   /** @type {string} */ var content;
   for (; length;) {
     element = document.getElementById(nodes[--length]);
-    content = element.textContent.trim();
+    content = element && element.textContent.trim();
 
     if (!element || !(PATTERN.test(content) || 'N/A' == content)) {
       return true;
