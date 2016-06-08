064cffe3008fc3a2dd93ef7202217088a75d5f3f
Oleksander Svirskiy <oleksandr.svirskyi@sethq.comoleksandr.svirskyi@sethq.comoleksandr.svirskyi@sethq.comoleksandr.svirskyi@sethq.com>
2016-06-08 17:18:18 +0300

    style fixed

--- tests/testmediaqueries.js
+++ tests/testmediaqueries.js
@@ -3,40 +3,39 @@
  */
 
 
-
 /**
  * @return {boolean} return true if test failed
  */
 function testMediaQueries() {
-    /** @type {string} */ var node = 'media-widget';
-    /** @type {Element} */ var element = document.getElementById(node);
-    /** @type {Element} */
-    var failClass = document.getElementsByClassName("rule media-none");
-    /** @const {!RegExp} */ var MEDIA_PATTERN = /@media([^{]*)/;
-    /** @type {Node} */ 
-    var table = document.getElementById('media-data-table');
-    /** @type {Element} */ var tableRows;
-    /** @type {number} */ var length;
-    /** @type {number} */ var i = 0;
+  /** @const {!RegExp} */ var MEDIA_PATTERN = /@media([^{]*)/;
+  /** @type {string} */ var node = 'media-widget';
+  /** @type {Element} */ var element = document.getElementById(node);
+  /** @type {Element} */
+  var failClass = document.getElementsByClassName('rule media-none');
+  /** @type {Node} */ var table = document.getElementById('media-data-table');
+  /** @type {number} */ var i = 0;
+  /** @type {Element} */ var tableRows;
+  /** @type {number} */ var length;
 
-    if (!node) {
+  if (!node) {
+    return true;
+  }
+  if (table) {
+    tableRows = element.getElementsByTagName('tr');
+    length = tableRows.length;
+
+    for (; i < length; i++) {
+      if (!MEDIA_PATTERN.test(tableRows[i].textContent.trim())) {
         return true;
+      }
     }
-    if (table) {
-        tableRows = element.getElementsByTagName("tr");
-        length = tableRows.length;
-
-        for (; i < length; i++) {
-            if (!MEDIA_PATTERN.test(tableRows[i].textContent.trim())) {
-                return true;
-            }
-        }
-    } else if (!failClass) {
-        return true;
-    }
-    return false;
-
+  } else if (!failClass) {
+    return true;
+  }
+  return false;
 }
 
+
 // Export for phantomjs.
+/** @type {!function():boolean} */
 window.testmediaqueries = testMediaQueries;
