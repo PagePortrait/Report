ab8435b314f36b0cdbfa802f8660c86469621965
Valentin <valentin@dtm.io>
2016-06-08 11:54:17 +0300

    Fixed jslint errors.

--- tests/testdemographics.js
+++ tests/testdemographics.js
@@ -6,58 +6,71 @@
 /** @const {!RegExp} */ var PATTERN = /^\d([,\.]*\d)*%$/;
 
 /** @const {!Object.<string, function(NodeList):boolean>} */ var VALIDATORS = {
-  'gender-data-table': function(elements) {
-    return testStaticDataTable(elements, ['male', 'female']);
+  /** @return {boolean} */ 'gender-data-table': function(elements) {
+    return testStaticDataTable_(elements, ['male', 'female']);
   },
-  'browsing-location-data-table': function(elements) {
-    return testStaticDataTable(elements, ['home', 'school', 'work']);
+  /** @return {boolean} */ 'browsing-location-data-table': function(elements) {
+    return testStaticDataTable_(elements, ['home', 'school', 'work']);
   },
-  'geo-data-table': function(elements) {
-    /** @type {number} */ var length = elements.length;
-    /** @type {number} */ var index = 0;
-    /** @type {Node} */ var table = document.getElementById('geo-data-table');
-    /** @type {Node} */ var parentNode = table && table.parentNode;
-    /** @type {string} */ var content;
-    /** @type {NodeList} */ var cells;
-
-    if (parentNode && ~parentNode.className.indexOf('true') &&
-        elements.length) {
-      return true;
-    }
-
-    for (; index < length; index++) {
-      cells = elements[index].querySelectorAll('th,td');
-      content = cells[1].textContent.trim();
-      if (!(cells[0].textContent.trim() &&
-          (PATTERN.test(content) || 'N/A' == content))) {
-        return false;
-      }
-    }
-    return true;
+  /** @return {boolean} */ 'geo-data-table': function(elements) {
+    return testGeoDataTable_(elements);
   }
 };
 
+
 /**
- * @return {boolean} Returns "true" if table is not valid.
+ * @param {NodeList} elements The table rows elements.
+ * @return {boolean} Returns "true" if data is not valid.
  */
-function testStaticDataTable(elements, headers) {
+function testGeoDataTable_(elements) {
   /** @type {number} */ var length = elements.length;
   /** @type {number} */ var index = 0;
+  /** @type {Node} */ var table = document.getElementById('geo-data-table');
+  /** @type {Node} */ var parentNode = table && table.parentNode;
   /** @type {string} */ var content;
   /** @type {NodeList} */ var cells;
 
-  if (length != headers.length) return true;
+  if (parentNode && ~parentNode.className.indexOf('true') &&
+      elements.length) {
+    return true;
+  }
 
   for (; index < length; index++) {
     cells = elements[index].querySelectorAll('th,td');
     content = cells[1].textContent.trim();
-    if (!(cells[0].textContent.trim() == headers[index] &&
+    if (!(cells[0].textContent.trim() &&
         (PATTERN.test(content) || 'N/A' == content))) {
       return false;
     }
   }
   return true;
 }
+
+
+/**
+ * @param {NodeList} elements The table rows elements.
+ * @param {!Array.<string>} metrics The metrics keys.
+ * @return {boolean} Returns "true" if data is not valid.
+ */
+function testStaticDataTable_(elements, metrics) {
+  /** @type {number} */ var length = elements.length;
+  /** @type {number} */ var index = 0;
+  /** @type {string} */ var content;
+  /** @type {NodeList} */ var cells;
+
+  if (length != metrics.length) return true;
+
+  for (; index < length; index++) {
+    cells = elements[index].querySelectorAll('th,td');
+    content = cells[1].textContent.trim();
+    if (!(cells[0].textContent.trim() == metrics[index] &&
+        (PATTERN.test(content) || 'N/A' == content))) {
+      return false;
+    }
+  }
+  return true;
+}
+
 
 /**
  * @return {boolean} Returns "true" if test failed.
@@ -66,14 +79,16 @@ function testDemographics() {
   for (/** @type {string} */ var metric in VALIDATORS) {
     /** @type {Element} */ var element = document.getElementById(metric);
     /* @type {function(NodeList):boolean} */ var validator = VALIDATORS[metric];
-    /** @type {NodeList} */ var content =
+    /** @type {NodeList} */ var elements =
         element && element.getElementsByTagName('tr');
-    if (content && !validator(content)) {
+    if (!elements.length || !validator(elements)) {
       return true;
     }
   }
   return false;
 }
 
+
 // Export for phantomjs.
+/** @type {!function():boolean} */
 window.testdemographics = testDemographics;
