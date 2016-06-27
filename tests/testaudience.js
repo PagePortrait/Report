298de19e5b22d888116ffd0024e68ba2fa1a71e8
Myhailo <myhailo@lwo-vl0020-53.corp.dtm.io>
2016-06-27 17:58:27 +0300

    small reworks

--- tests/testaudience.js
+++ tests/testaudience.js
@@ -3,7 +3,7 @@
  * Success criterias:
  * - each element from (nodes) array should exist;
  * - content of the array element should exist and match the PATTERN;
- * - content of the array element can't be equal to 'N/A'.
+ * - content of the array element can not be equal to 'N/A'.
  * @see http://google.github.io/styleguide/javascriptguide.xml
  * @see http://developers.google.com/closure/compiler/docs/js-for-compiler
  */
