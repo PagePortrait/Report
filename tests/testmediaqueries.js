44613f981f07879b8013da0ff77758921c8dd974
Myhailo <myhailo@lwo-vl0020-53.corp.dtm.io>
2016-06-27 18:16:19 +0300

    success criterias

--- tests/testmediaqueries.js
+++ tests/testmediaqueries.js
@@ -1,34 +1,41 @@
 /**
  * @fileoverview Defines tests for testMediaQueries.
+ * Success criterias:
+ * - element with id 'media-widget' should exist;
+ * - if widget is not empty it should contain element with id
+ *  'media-data-table';
+ * - rows in 'media-data-table' should contain text string '@media';
+ * - if widget is empty it should contain class '.rule.media-none'.
+ * @see http://google.github.io/styleguide/javascriptguide.xml
+ * @see http://developers.google.com/closure/compiler/docs/js-for-compiler
  */
+
+
+/** @const {!RegExp} */ var MEDIA_PATTERN = /@media([^{]*)/;
+/** @const {string} */ var CSS_RULE = '.rule.media-none';
+/** @const {string} */ var TABLE_ID = 'media-data-table';
 
 
 /**
  * @return {boolean} Returns "true" if test failed.
  */
-
-/** @const {!RegExp} */ var MEDIA_PATTERN = /@media([^{]*)/;
-
 function testMediaQueries() {
-  /** @type {Element} */ 
-  var element = document.getElementById('media-widget');
-  /** @type {NodeList} */ var rule = element && 
-      element.getElementsByClassName('rule media-none');
-  /** @type {Element} */ 
-  var table = document.getElementById('media-data-table');
-  /** @type {number} */ var i = 0;
+  /** @type {Element} */ var element = document.getElementById('media-widget');
+  /** @type {Element} */ var rule = element && element.querySelector(CSS_RULE);
+  /** @type {Element} */ var table = document.getElementById(TABLE_ID);
   /** @type {NodeList} */ var rows;
   /** @type {number} */ var length;
 
   if (!element) {
     return true;
   }
+
   if (table) {
-    rows = element && element.getElementsByTagName('tr');
+    rows = element.getElementsByTagName('TR');
     length = rows.length;
 
-    for (; i < length;) {
-      if (!MEDIA_PATTERN.test(rows[i++].textContent.trim())) {
+    for (; length;) {
+      if (!MEDIA_PATTERN.test(rows[--length].textContent.trim())) {
         return true;
       }
     }
