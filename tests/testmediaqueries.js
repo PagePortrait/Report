abe9f98868697ccc7de8013ad3854b38a57d22d0
Roman <roman@lwo-vl0020-70.corp.dtm.io>
2016-06-29 18:24:51 +0300

    fixed

--- tests/testmediaqueries.js
+++ tests/testmediaqueries.js
@@ -20,6 +20,7 @@
  * @return {boolean} Returns "true" if test failed.
  */
 function testMediaQueries() {
+  /** @type {boolean} */ var result = false;
   /** @type {Element} */ var element = document.getElementById('media-widget');
   /** @type {Element} */ var rule = element && element.querySelector(CSS_RULE);
   /** @type {Element} */ var table = document.getElementById(TABLE_ID);
@@ -33,7 +34,7 @@ function testMediaQueries() {
 
     for (; length;) {
       if (MEDIA_PATTERN.test(rows[--length].textContent.trim()) || rule) {
-        return false;
+        return result;
       }
     }
   }
