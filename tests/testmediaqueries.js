8620494a4f655ec60ae431389e496af2a9801261
Roman <roman@lwo-vl0020-70.corp.dtm.io>
2016-07-04 16:08:02 +0300

    reworked

--- tests/testmediaqueries.js
+++ tests/testmediaqueries.js
@@ -12,6 +12,7 @@
 
 
 /** @const {!RegExp} */ var MEDIA_PATTERN = /@media([^{]*)/;
+/** @const {string} */ var WIDGET = 'media-widget';
 /** @const {string} */ var CSS_RULE = '.rule.media-none';
 /** @const {string} */ var TABLE_ID = 'media-data-table';
 
@@ -20,27 +21,23 @@
  * @return {boolean} Returns "true" if test failed.
  */
 function testMediaQueries() {
-  /** @type {boolean} */ var result = false;
-  /** @type {Element} */ var element = document.getElementById('media-widget');
+  /** @type {Element} */ var element = document.getElementById(WIDGET);
   /** @type {Element} */ var rule = element && element.querySelector(CSS_RULE);
+  /** @type {boolean} */ var result = !rule;
   /** @type {Element} */ var table = document.getElementById(TABLE_ID);
   /** @type {NodeList} */ var rows;
   /** @type {number} */ var length;
 
-
   if (element && table) {
     rows = element.getElementsByTagName('TR');
     length = rows.length;
-
     for (; length;) {
-      if (!MEDIA_PATTERN.test(rows[--length].textContent.trim())) {
-        result = true;
-        break;
+      if (MEDIA_PATTERN.test(rows[--length].textContent.trim())) {
+        result = false;
       }
     }
-  } else if (!rule) {
-    result = true;
   }
+
   return result;
 }
 
