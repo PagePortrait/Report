9f19181af7913f49310b6cae43fda1052a4caecc
Roman <roman@lwo-vl0020-70.corp.dtm.io>
2016-07-06 15:07:57 +0300

    fixed

--- tests/testmediaqueries.js
+++ tests/testmediaqueries.js
@@ -1,19 +1,23 @@
 /**
  * @fileoverview Defines test for Media Queries widget.
  * Success criterias:
- * - Element with id 'media-widget' should exist;
- * - If widget is not empty it should contain element with id
- *   'media-data-table' and shouldn't contain '.rule.media-none';
- * - Every row in 'media-data-table' should contain text string '@media';
- * - If widget is empty it should contain classes '.rule.media-none' and
- *   shouldn't containe 'media-data-table'.
+ * - Widget with ID 'media-widget' should exist and can be empty or can
+ *   return media queries;
+ * - If widget is not empty it should contain element with ID
+ *   'media-data-table' and shouldn't contain element with
+ *   CSS classes '.rule.media-none';
+ * - If widget contains element with ID 'media-data-table' then every
+ *   row in 'media-data-table' should contain text string '@media';
+ * - If widget is empty it should contain element with
+ *   CSS classes '.rule.media-none' and shouldn't contain element
+ *   with ID 'media-data-table'.
  * @see http://google.github.io/styleguide/javascriptguide.xml
  * @see http://developers.google.com/closure/compiler/docs/js-for-compiler
  */
 
 
 /** @const {!RegExp} */ var MEDIA_PATTERN = /@media([^{]*)/;
-/** @const {string} */ var WIDGET = 'media-widget';
+/** @const {string} */ var WIDGET_ID = 'media-widget';
 /** @const {string} */ var CSS_RULE = '.rule.media-none';
 /** @const {string} */ var TABLE_ID = 'media-data-table';
 
@@ -22,8 +26,9 @@
  * @return {boolean} Returns "true" if test failed.
  */
 function testMediaQueries() {
-  /** @type {Element} */ var element = document.getElementById(WIDGET);
-  /** Element with id 'media-widget' should exist. */
+  /** @type {Element} */ var element = document.getElementById(WIDGET_ID);
+  /**  Widget with ID 'media-widget' should exist and can be empty or can
+       return media queries. */
   /** @type {Element} */ var table = document.getElementById(TABLE_ID);
   /** @type {boolean} */
   var result = element && element.querySelector(CSS_RULE);
@@ -31,22 +36,24 @@ function testMediaQueries() {
   /** @type {number} */ var length;
 
   if (!result && table) {
-    /** If widget is not empty it should contain element with id
-        'media-data-table' and shouldn't contain '.rule.media-none'. */
+    /** If widget is not empty it should contain element with ID
+        'media-data-table' and shouldn't contain element with
+        CSS classes '.rule.media-none'. */
     rows = element.getElementsByTagName('TR');
     length = rows.length;
     for (; length;) {
       if (!MEDIA_PATTERN.test(rows[--length].textContent.trim())) {
-        /** Every row in 'media-data-table' should contain text
-            string '@media'. */
+        /** If widget contains element with ID 'media-data-table' then every
+            row in 'media-data-table' should contain text string '@media'. */
         result = true;
         break;
       }
     }
   }
   if (result && !table) {
-    /** If widget is empty it should contain classes '.rule.media-none' and
-        shouldn't containe 'media-data-table'. */
+    /** If widget is empty it should contain element with
+        CSS classes '.rule.media-none' and shouldn't contain element
+        with ID 'media-data-table' */
     result = false;
   }
 
