7418b79f2d6c139ebd634b633132f3af9b024775
Roman <roman@lwo-vl0020-70.corp.dtm.io>
2016-07-07 17:24:03 +0300

    rewrite success criteris

--- tests/testoptionsbutton.js
+++ tests/testoptionsbutton.js
@@ -2,14 +2,15 @@
  * @fileoverview Defines tests for Options button.
  * Success criteria:
  * - Element with CSS class 'report-actions' should exist;
- * - Element with CSS class 'report-actions' should contain element 'li';
- * - Element 'li' should have CSS class 'closed' by default;
+ * - Element with CSS class 'report-actions' should contain element with
+ *   tag name 'li';
+ * - Element with tag name 'li' should have CSS class 'closed' by default;
  * - Element with CSS class 'report-actions' should contain element with
  *   ID 'toggle-widgets-view';
  * - Element with CSS class 'report-actions' should be clickable;
- * - On click on element with CSS class 'report-actions' to all element with
+ * - Upon click on element with CSS class 'report-actions' to all elements with
  *   CSS class 'is-expandable' should be added CSS class 'expanded';
- * - On second click on element with CSS class 'report-actions' in all element
+ * - On second click on element with CSS class 'report-actions' in all elements
  *   with CSS class 'is-expandable' should be removed CSS class 'expanded'.
  * @see http://google.github.io/styleguide/javascriptguide.xml
  * @see http://developers.google.com/closure/compiler/docs/js-for-compiler
@@ -31,10 +32,11 @@ function testOptionsButton() {
   /** @type {Element} */
   // Element with CSS class 'report-actions' should exist.
   var optionsButton = document.querySelector(BUTTON_CLASS_NAME);
-  // Element with CSS class 'report-actions' should contain element 'li'.
+  // Element with CSS class 'report-actions' should contain element with
+  // tag name'li'.
   /** @type {Element} */
   var element = optionsButton.getElementsByTagName('li')[0];
-  // Element 'li' should have CSS class 'closed' by default.
+  // Element with tag name 'li' should have CSS class 'closed' by default.
   /** @type {Node} */
   var buttonClosed = element &&
       element.getElementsByClassName(BUTTON_CLOSED_CLASS_NAME);
@@ -51,12 +53,12 @@ function testOptionsButton() {
     if (optionsButton.dispatchEvent(new Event('click'))) {
       toggleButton.dispatchEvent(new Event('click'));
       expanded = document.querySelectorAll(EXPANDED_CLASS_NAME);
-      // On click on element with CSS class 'report-actions' to all element with
-      // CSS class 'is-expandable' should be added CSS class 'expanded'.
+      // Upon click on element with CSS class 'report-actions' to all elements
+      // with CSS class 'is-expandable' should be added CSS class 'expanded'.
       if (expanded) {
         toggleButton.dispatchEvent(new Event('click'));
         // On second click on element with CSS class 'report-actions' in all
-        // element with CSS class 'is-expandable' should be removed
+        // elements with CSS class 'is-expandable' should be removed
         // CSS class 'expanded'.
         if (!expanded.length) {
           result = false;
