9771cbbb1aa8bd95bb6f38186e5f20b4ec0a5b92
Roman <roman@lwo-vl0020-70.corp.dtm.io>
2016-07-08 13:26:15 +0300

    reworked webfiles test

--- tests/testwebfiles.js
+++ tests/testwebfiles.js
@@ -1,9 +1,16 @@
 /**
  * @fileoverview Defines tests for web files.
  * Successful criterias:
- * - Web Files widget should exist;
- * - Elements "h4" should exist in web files;
- * - In "h4" should be HTTP status code and its type should number.
+ * - Element with ID 'webfiles-container' should exist;
+ * - Element with ID 'webfiles-container' should contain elements with
+ *   tag name 'h4';
+ * - In element with tag name 'h4' should be one of the next HTTP status code:
+ *   0, 100, 101, 102, 200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300,
+ *   301, 302, 303, 304, 305, 306, 307, 308, 400, 401, 402, 403, 404, 405, 406,
+ *   407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420, 421, 422,
+ *   423, 424, 426, 428, 429, 431, 440, 444, 449, 450, 451, 495, 496, 497, 498,
+ *   499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 520, 521,
+ *   522, 523, 524, 525, 526, 530;
  * @see http://google.github.io/styleguide/javascriptguide.xml
  * @see http://developers.google.com/closure/compiler/docs/js-for-compiler
  */
@@ -17,6 +24,7 @@
   499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 520, 521,
   522, 523, 524, 525, 526, 530
 ];
+/** @const {string} */ var WIDGET_ID = 'webfiles-container';
 
 
 /**
@@ -25,18 +33,27 @@
 function testWebFiles() {
   /** @type {boolean} */ var result = true;
   /** @type {Element} */
-  var container = document.getElementById('webfiles-container');
+  var container = document.getElementById(WIDGET_ID);
   /** @type {NodeList} */
   var elements = container && container.querySelectorAll('h4');
   /** @type {number} */ var length = container && elements.length;
   /** @type {number} */ var code;
   /** @type {string} */ var text;
 
-
+  // Element with ID 'webfiles-container' should exist;
+  // Element with ID 'webfiles-container' should contain elements with
+  // tag name 'h4'.
   if (container && length) {
     for (; length;) {
       text = elements[--length].textContent.trim();
       code = +text.split(': ')[1];
+      // In element with tag name 'h4' should be one of the next HTTP status
+      // code: 0, 100, 101, 102, 200, 201, 202, 203, 204, 205, 206, 207, 208,
+      // 226, 300, 301, 302, 303, 304, 305, 306, 307, 308, 400, 401, 402, 403,
+      // 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417,
+      // 418, 420, 421, 422, 423, 424, 426, 428, 429, 431, 440, 444, 449, 450,
+      // 451, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504, 505, 506, 507,
+      // 508, 509, 510, 511, 520, 521, 522, 523, 524, 525, 526, 530.
       if (!isNaN(code) || ~HTTP_STATUS_CODES.indexOf(code)) {
         result = false;
       }
