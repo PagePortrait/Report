c4baff70e23a88a074297fa380e961eaa1563080
Roman <roman@lwo-vl0020-70.corp.dtm.io>
2016-07-11 13:44:19 +0300

    rewrote success criteria

--- tests/testaudience.js
+++ tests/testaudience.js
@@ -1,35 +1,12 @@
 /**
  * @fileoverview Defines test for Audience engagement.
  * Success criteria:
- * - Elements with ID 'audience-engagement-visitors-value',
- *   'audience-engagement-pageviews-value',
- *   'audience-engagement-pageviews-delta',
- *   'audience-engagement-time-on-site-value',
- *   'audience-engagement-time-on-site-delta',
- *   'audience-engagement-search-visits-value',
- *   'audience-engagement-search-visits-delta',
- *   'audience-engagement-bounce-rate-value',
- *   'audience-engagement-bounce-rate-delta' should exist;
- * - Elements with ID 'audience-engagement-visitors-value',
- *   'audience-engagement-pageviews-value',
- *   'audience-engagement-pageviews-delta',
- *   'audience-engagement-time-on-site-value',
- *   'audience-engagement-time-on-site-delta',
- *   'audience-engagement-search-visits-value',
- *   'audience-engagement-search-visits-delta',
- *   'audience-engagement-bounce-rate-value',
- *   'audience-engagement-bounce-rate-delta' should contain content and
+ * - Elements with ID prefix 'audience-engagement-‘ should exist;
+ * - Elements with ID prefix 'audience-engagement-‘ should contain content and
  *   the format should adhere to the following examples: 10.1% or 10% or
  *   10:1 or 10”;
- * - Content of elements with ID 'audience-engagement-visitors-value',
- *   'audience-engagement-pageviews-value',
- *   'audience-engagement-pageviews-delta',
- *   'audience-engagement-time-on-site-value',
- *   'audience-engagement-time-on-site-delta',
- *   'audience-engagement-search-visits-value',
- *   'audience-engagement-search-visits-delta',
- *   'audience-engagement-bounce-rate-value',
- *   'audience-engagement-bounce-rate-delta' can't equal 'N/A'.
+ * - Content of elements with ID prefix 'audience-engagement-‘ can't
+ *   equal 'N/A'.
  * @see http://google.github.io/styleguide/javascriptguide.xml
  * @see http://developers.google.com/closure/compiler/docs/js-for-compiler
  */
@@ -42,15 +19,7 @@
  * @return {boolean} Returns "true" if test failed.
  */
 function testAudience() {
-  // Elements with ID 'audience-engagement-visitors-value',
-  //   'audience-engagement-pageviews-value',
-  //   'audience-engagement-pageviews-delta',
-  //   'audience-engagement-time-on-site-value',
-  //   'audience-engagement-time-on-site-delta',
-  //   'audience-engagement-search-visits-value',
-  //   'audience-engagement-search-visits-delta',
-  //   'audience-engagement-bounce-rate-value',
-  //   'audience-engagement-bounce-rate-delta' should exist.
+  // Elements with ID prefix 'audience-engagement-‘ should exist.
   /** @type {!Array.<string>} */ var nodes = [
     'audience-engagement-visitors-value',
     'audience-engagement-pageviews-value',
@@ -71,18 +40,11 @@ function testAudience() {
     element = document.getElementById(nodes[--length]);
     content = element && element.textContent.trim();
 
-    // Elements with ID 'audience-engagement-visitors-value',
-    //   'audience-engagement-pageviews-value',
-    //   'audience-engagement-pageviews-delta',
-    //   'audience-engagement-time-on-site-value',
-    //   'audience-engagement-time-on-site-delta',
-    //   'audience-engagement-search-visits-value',
-    //   'audience-engagement-search-visits-delta',
-    //   'audience-engagement-bounce-rate-value',
-    //   'audience-engagement-bounce-rate-delta' should contain content and
-    //   the format should adhere to the following examples: 10.1% or 10% or
-    //   10:1 or 10”;
-    //   Content can't can't equal 'N/A'.
+    // Elements with ID prefix 'audience-engagement-‘ should contain content and
+    // the format should adhere to the following examples: 10.1% or 10% or
+    // 10:1 or 10”
+    // Content of elements with ID prefix 'audience-engagement-‘ can't
+    // equal 'N/A'.
     if (!content || !(PATTERN.test(content) || 'N/A' == content)) {
       result = true;
       break;
