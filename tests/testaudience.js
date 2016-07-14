8e112a7c028d9777bded1a14ab749246263dbbb2
Roman <roman@lwo-vl0020-70.corp.dtm.io>
2016-07-14 11:32:29 +0300

    fixed comments

--- tests/testaudience.js
+++ tests/testaudience.js
@@ -5,8 +5,8 @@
  * - Elements with ID prefix 'audience-engagement-‘ should contain content and
  *   the format should adhere to the following examples: 10.1% or 10% or
  *   10:1 or 10;
- * - Content of elements with ID prefix 'audience-engagement-‘ can't
- *   equal 'N/A'.
+ * - If in elements with ID prefix 'audience-engagement-‘ isn't data, 
+ *   content should be equal 'N/A'.
  * @see http://google.github.io/styleguide/javascriptguide.xml
  * @see http://developers.google.com/closure/compiler/docs/js-for-compiler
  */
@@ -43,9 +43,9 @@ function testAudience() {
     // Elements with ID prefix 'audience-engagement-‘ should contain content 
     // and the format should adhere to the following examples: 10.1% or 10% or
     // 10:1 or 10;
-    // Content of elements with ID prefix 'audience-engagement-‘ can't
-    // equal 'N/A'.
-    if (!content || !(PATTERN.test(content) || 'N/A' == content)) {
+    // If in elements with ID prefix 'audience-engagement-‘ isn't data, 
+    // content should be equal 'N/A'
+    if (!(content && (PATTERN.test(content) || 'N/A' == content))) {
       result = true;
       break;
     }
