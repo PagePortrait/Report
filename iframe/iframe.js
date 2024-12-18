1f159def4262cf2b5188bd7a23b46a0bdc8fa521
Valentin Podkamennyi <vpodk@users.noreply.github.com>
2024-12-18 14:47:09 -0800

    Update iframe.js

--- iframe/iframe.js
+++ iframe/iframe.js
@@ -45,11 +45,11 @@
     return iframe;
   };
 
-  window.addEventListener("load", (event) => {
-    console.log("[PARENT] load:event:", event);
-    const iframe = getCookieIframe();
-    iframe.contentWindow.postMessage({ action: "handshake" }, location.origin);
-  });
+  // window.addEventListener("load", (event) => {
+  //   console.log("[PARENT] load:event:", event);
+  //   const iframe = getCookieIframe();
+  //   iframe.contentWindow.postMessage({ action: "handshake" }, location.origin);
+  // });
 
   window.addEventListener("message", (event) => {
     console.log("[PARENT] message:event:", event);
@@ -63,7 +63,7 @@
         console.log("[PARENT] HANDSHAKE_ACTION");
         iframe.contentWindow.postMessage(
           { action: actions.INCREMENT_VIEWS_ACTION },
-          "*" // location.origin
+          IFRAME_ORIGIN // "*" // location.origin
         );
       } else if (action === actions.INCREMENT_VIEWS_ACTION) {
         console.log("[PARENT] INCREMENT_VIEWS_ACTION");
